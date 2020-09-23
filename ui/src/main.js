b4819d823f422da74c36f754b1b8b168bc642389
kwlee <kwlee@jiran.com>
2020-09-23 18:23:52 +0900

    1

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { createApp } from "vue";
+import * as antd from "ant-design-vue";
 
-createApp(App).mount('#app')
+// import { routes } from '.';
+import routes from "@/router";
+import App from "./App.vue";
+import store from "@/store";
+
+import "ant-design-vue/dist/antd.css";
+
+const routerHistory = createWebHistory();
+const router = createRouter({
+  history: routerHistory,
+  routes: routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.use(antd);
+app.mount("#app");
